453a2a01d60d8a59653c2461bf9d18449a3325bb
Tate Fishburn <d00444216@utahtech.edu>
2026-02-26 10:08:15 -0700

    added for loops

---
+++ for_loops.py
@@ -0,0 +1,19 @@
+# Write a Python program that generates and prints all of the C(5,3), 
+# all the CR(5,3), all the P(5,3), and all the PR(5,3) using nested for loops.
+# 5, 3
+
+# Combinations: n!/(n-r)!r!
+# CombinationsR: C(n+r-1, r)
+# Permutations: n!/(n-r)!
+def printCombinations(n, r):
+    for i in range(r):
+        
+
+def printCombinationsR(n, r):
+    return
+
+def printPermutations(n, r):
+    return
+
+if __name__ == "__main__":
+    printCombinations(5, 3)
